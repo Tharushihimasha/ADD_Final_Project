f1a1e025360cc8de1c010c928f1f6b3c6d4fe984
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2023-11-01 20:44:23 +0530

    init commit

---
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/guide_micro_service/GuideMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse63.guide_micro_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuideMicroServiceApplication.class, args);
+    }
+
+}
---
+++ Guide Micro Service/src/test/java/lk/ijse/gdse63/guide_micro_service/GuideMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse63.guide_micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
